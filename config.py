7625b3ee10989911999b0fdade36917aeb5266c1
0neye <sidusmatter@gmail.com>
2024-06-02 11:39:01 -0800

    N/A

--- config.py
+++ config.py
@@ -35,6 +35,7 @@ PERCENT_ACCEPT = 0.9 # % required to approve a request
 VOTE_TIME_PERIOD = 60 # * 60 * 60 * 24 * 7  # 7 days in seconds
 CHECK_TIME = 50 # how often you want to check if the voting period has ended; in seconds
 
+
 CHANNEL_ID = 0  # Forum channel ID
 STATE_FILE_NAME = 'requests_state.json'
 DEV_MODE = False # for ease of testing, turns off many checks
